525d2102f5e94e1ff4e570a447bc8e188f097796
Terashima <yamanogomora@gmail.com>
2019-01-24 08:52:48 +0900

    #08 prepareメソッドを実装していこう

--- MyMemoApp/MemoViewController.swift
+++ MyMemoApp/MemoViewController.swift
@@ -13,6 +13,7 @@ class MemoViewController: UIViewController {
     var memo: String?
     
     @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +27,10 @@ class MemoViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        guard let button = sender as? UIBarButtonItem, button === self.saveButton else {
+            return
+        }
+        self.memo = self.memoTextField.text ?? ""
     }
     
 
