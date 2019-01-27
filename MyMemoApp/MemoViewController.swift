544464ed093bd04edaf4e6bf3e950764b1d057bc
Terashima <yamanogomora@gmail.com>
2019-01-27 09:09:15 +0900

    #15 キャンセルボタンを動作させよう

--- MyMemoApp/MemoViewController.swift
+++ MyMemoApp/MemoViewController.swift
@@ -19,19 +19,29 @@ class MemoViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.saveButton.isEnabled = false
         if let memo = self.memo {
             self.memoTextField.text = memo
+            self.navigationItem.title = "Edit Memo"
         }
+        updateSaveButtonState()
     }
-
-    @IBAction func memoTextFieldChanged(_ sender: Any) {
+    
+    private func updateSaveButtonState() {
         let memo = self.memoTextField.text ?? ""
         self.saveButton.isEnabled = !memo.isEmpty
     }
+
+    @IBAction func memoTextFieldChanged(_ sender: Any) {
+        updateSaveButtonState()
+    }
     
     @IBAction func cancel(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        if self.presentingViewController is UINavigationController {
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
